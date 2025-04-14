58e1e065b128776a6388b9469f575abb571bf214
galeed gutierrez <galeedeveloper@gmail.com>
2025-04-14 03:43:16 -0300

    fix: standardize end-of-line handling in Prettier configuration

--- eslint.config.js
+++ eslint.config.js
@@ -56,7 +56,7 @@ export default defineConfig([
 			unicorn: eslintPluginUnicorn,
 		},
 		rules: {
-			'prettier/prettier': ['error', { endOfLine: 'auto' }],
+			'prettier/prettier': 'error',
 
 			// Error prevention
 			'array-callback-return': ['error', { checkForEach: true }],
