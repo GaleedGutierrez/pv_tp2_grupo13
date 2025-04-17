448f4ad81822a8709c4ad2304b555cac65426b10
Alfredo Ezequiel Gonzalez Lopez <eg156765@gmail.com>
2025-04-17 15:53:03 -0300

    feat: desarrollo ejercicio1

---
+++ js/ejercicio1.js
@@ -0,0 +1,4 @@
+const integrantes =
+	'Enzo Isaias Condori, Franklin Fernando Alexander Vazquez, Tomas Leandro Tastaca Gutierrez, Axel Enrique Galeed Gutierrez, Alfredo Ezequiel Gonzalez Lopez';
+
+console.info(integrantes);
