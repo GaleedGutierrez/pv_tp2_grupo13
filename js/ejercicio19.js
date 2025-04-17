ec3baffb010f6f3344b9c9b856b4ac31f962c339
galeed gutierrez <galeedeveloper@gmail.com>
2025-04-17 20:14:39 -0300

    refactor: improve formatting and structure of ejercicio19 HTML and JavaScript files

--- js/ejercicio19.js
+++ js/ejercicio19.js
@@ -1,21 +1,38 @@
-export function obtenerDatos() {
-    const nombre = document.querySelector('#nombre').value;
-    const apellido = document.querySelector('#apellido').value;
-    const libreta = document.querySelector('#libreta').value;
+/**
+ * Obtiene los datos ingresados en los campos de nombre, apellido y libreta.
+ *
+ * @returns {{ nombre: string, apellido: string, libreta: string }} Objeto con los datos del formulario.
+ */
+function obtenerDatos() {
+	const nombre = document.querySelector('#nombre').value;
+	const apellido = document.querySelector('#apellido').value;
+	const libreta = document.querySelector('#libreta').value;
 
-    return { nombre, apellido, libreta };
+	return { nombre, apellido, libreta };
 }
 
-export function mostrarDatos(datos) {
-    alert(`Los datos ingresados son:
+/**
+ *  Muestra los datos ingresados en una alerta.
+ * */
+function mostrarDatos(datos) {
+	alert(`Los datos ingresados son:
     Nombre: ${datos.nombre}
     Apellido: ${datos.apellido}
     Libreta Universitaria: ${datos.libreta}`);
 }
 
-export function manejarFormulario(event) {
-    event.preventDefault(); 
-    const datos = obtenerDatos();
-    mostrarDatos(datos);
+/**
+ *
+ * @param {Event} event
+ */
+function manejarFormulario(event) {
+	event.preventDefault();
+
+	const datos = obtenerDatos();
+
+	mostrarDatos(datos);
 }
-document.querySelector('#formulario').addEventListener('submit', manejarFormulario);
+
+document
+	.querySelector('#formulario')
+	.addEventListener('submit', manejarFormulario);
